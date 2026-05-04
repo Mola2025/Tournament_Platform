e249ed057e4cc4db631d39e063eb493272417df5
David Molano <davidm0l4n0@gmail.com>
2026-05-03 21:32:29 -0400

    Patch App.js Again

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,7 +17,7 @@ app.use("/matches", matchRoutes);
 // To fix the issue that the navbar is was showing and now the frontend is not showing, we need to serve the frontend from the backend
 const frontendPath = path.join(__dirname, "../frontend/dist/frontend/browser");
 app.use(express.static(frontendPath));
-app.get("*", (req, res) => {
+app.get("/{*any}", (req, res) => {
   res.sendFile(path.join(frontendPath, "index.html"));
 });
 
