595f12d537d3a8a59a64824e68458a34c77cdcad
David Molano <davidm0l4n0@gmail.com>
2026-05-03 21:36:36 -0400

    Rollback Patch Compleatly to keep the navbar and have a correct deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const path = require("path");
 const authRoutes = require("./routes/AuthenticationRoutes");
 const tournamentRoutes = require("./routes/TournamentRoutes");
 const matchRoutes = require("./routes/MatchesRoutes");
@@ -13,12 +12,5 @@ app.use(cors());
 app.use("/auth", authRoutes);
 app.use("/tournaments", tournamentRoutes);
 app.use("/matches", matchRoutes);
-
-// To fix the issue that the navbar is was showing and now the frontend is not showing, we need to serve the frontend from the backend
-const frontendPath = path.join(__dirname, "../frontend/dist/frontend/browser");
-app.use(express.static(frontendPath));
-app.get("/{*any}", (req, res) => {
-  res.sendFile(path.join(frontendPath, "index.html"));
-});
 
 module.exports = app;
